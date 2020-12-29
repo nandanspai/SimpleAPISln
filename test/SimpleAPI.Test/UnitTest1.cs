a8231043131bea8b2b707a363ad32243624af865
Nandan Pai <npai@vitalhealthsoftware.com>
2020-12-29 12:25:56 +0100

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController weatherForecastController = new WeatherForecastController(null);
+
+        [Fact]
+        public void WeatherForecastController_GET_Returns5Results()
+        {
+            int expectedValue = 5;
+            IEnumerable<WeatherForecast> result = weatherForecastController.Get();
+            Assert.Equal(expectedValue, result.Count());
+        }
+    }
+}
